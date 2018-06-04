832f311d1db5bdb0b0242ed31c2375b7fc00e573
pedr0rocha <pedr0_r@hotmail.com>
2018-06-04 00:52:43 -0300

    Generating jwt on register and login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const authConfig = require('../config/auth.json');
 
 const User = require('../models/user.js');
 
 const router = express.Router();
 
+function generateJwt(userId) {
+	return jwt.sign({ id: userId }, authConfig.secret, { expiresIn: 86400 });
+}
 
 router.post('/register', async (req, res) => {
-	console.log(req.body);
 	const { email } = req.body;
 
 	try {
@@ -16,8 +21,12 @@ router.post('/register', async (req, res) => {
 		const user = await User.create(req.body);
 		user.password = undefined;
 
+		console.log("New user:");
 		console.log(user);
-		return res.send({ user });
+		return res.send({ 
+			user, 
+			token: generateJwt(user.id)
+		});
 	} catch (err) {
 		console.log(err);
 		return res.status(400).send({ error: 'Registration failed'});
@@ -33,7 +42,13 @@ router.post('/login', async (req, res) => {
 	if (!await bcrypt.compare(password, user.password)) return res.status(400).send({ error: 'Invalid password '});
 
 	user.password = undefined;
-	res.send({ user });
+
+	console.log("Logged in:");
+	console.log(user);
+	res.send({ 
+		user, 
+		token: generateJwt(user.id)
+	});
 });
 
 module.exports = app => app.use('/auth', router);
